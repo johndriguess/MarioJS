2ee1366122f89f4999bc7c4dc99a5e4d59cb6ba6
Johnatan Rodrigues <johndriguess@gmail.com>
2023-10-20 21:41:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const clouds = document.querySelector('.clouds');
 const restart = document.querySelector('.restart');
 
 let gameOver = false;
+let loop;
+let lastJumpTime = 0;
+const jumpCooldown = 500;
 
 const jump = () => {
     if(!gameOver){
@@ -15,46 +18,58 @@ const jump = () => {
 }
 
 const restartGame = () => {
-    location.reload()
+    location.reload();
 }
 
 const endGame = () => {
     gameOver = true; 
     restart.style.display = 'block'; 
+    clouds.style.animationPlayState = 'paused';
+    pipe.style.animationPlayState = 'paused';
+    clearInterval(loop);
 }
 
-const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).
-    bottom.replace('px','');
-    const cloudsPosition = clouds.offsetLeft;
-    
+function startGameLoop() {
+    if (!loop && !gameOver) {
+        loop = setInterval(() => {
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+            const cloudsPosition = clouds.offsetLeft;
 
-    if(pipePosition <= 120 && marioPosition < 80 && pipePosition > 0){
-        pipe.style.animationPlayState = 'paused';
-        pipe.style.left = `${pipePosition}px`
+            if(pipePosition <= 120 && marioPosition < 80 && pipePosition > 0){
+                pipe.style.animationPlayState = 'paused';
+                pipe.style.left = `${pipePosition}px`
 
-        mario.classList.remove('.jump')
-        mario.style.bottom = `${marioPosition}px`
-        mario.src = './images/game-over.png'
-        mario.style.width = '80px'
-        mario.style.marginLeft = '50px'
+                mario.classList.remove('.jump')
+                mario.style.bottom = `${marioPosition}px`
+                mario.src = './images/game-over.png'
+                mario.style.width = '80px'
+                mario.style.marginLeft = '50px'
 
-        clouds.style.animationPlayState = 'paused';
-        clouds.style.left = cloudsPosition+'px';
-        clearInterval(loop);
-        endGame();
-        
+                clouds.style.animationPlayState = 'paused';
+                clouds.style.left = cloudsPosition+'px';
+                clearInterval(loop);
+                endGame();
+            }
+        }, 10);
     }
-},10)
+}
 
 
+
+
+startGameLoop();
 
 document.addEventListener('keydown', (event) => {
     if ((event.key === 'R' || event.key === 'r') && gameOver) {
+        console.log("Tecla 'R' pressionada para reiniciar o jogo");
         restartGame();
     } else {
-        jump();
+        const currentTime = new Date().getTime();
+        if (currentTime - lastJumpTime >= jumpCooldown) {
+            jump();
+            lastJumpTime = currentTime;
+        }
     }
 });
 
